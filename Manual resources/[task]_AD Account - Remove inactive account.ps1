03210dae7b21766ee9641ce4ee3f7ef3f3ed697a
Jerry Breek <j.breek@tools4ever.com>
2022-08-02 16:58:07 +0200

    Updated with code for SA agent and audit logging

--- Manual resources/[task]_AD Account - Remove inactive account.ps1
+++ Manual resources/[task]_AD Account - Remove inactive account.ps1
@@ -11,7 +11,27 @@ catch {
 try {
     Remove-ADObject -Identity $adUser.DistinguishedName -Recursive -Confirm:$false
     write-information "Finished deleting AD user [$userPrincipalName]"
+    $Log = @{
+            Action            = "DeleteAccount" # optional. ENUM (undefined = default) 
+            System            = "ActiveDirectory" # optional (free format text) 
+            Message           = "Successfully deleted AD user $userPrincipalName" # required (free format text) 
+            IsError           = $false # optional. Elastic reporting purposes only. (default = $false. $true = Executed action returned an error) 
+            TargetDisplayName = $adUser.Name # optional (free format text) 
+            TargetIdentifier  = $([string]$adUser.SID) # optional (free format text) 
+        }
+        #send result back  
+        Write-Information -Tags "Audit" -MessageData $log
 }
 catch {
     write-error "Could not delete AD user [$userPrincipalName]. Error: $($_.Exception.Message)" 
+    $Log = @{
+            Action            = "DeleteAccount" # optional. ENUM (undefined = default) 
+            System            = "ActiveDirectory" # optional (free format text) 
+            Message           = "Failed to delete AD user $userPrincipalName" # required (free format text) 
+            IsError           = $true # optional. Elastic reporting purposes only. (default = $false. $true = Executed action returned an error) 
+            TargetDisplayName = $adUser.Name # optional (free format text) 
+            TargetIdentifier  = $([string]$adUser.SID) # optional (free format text) 
+        }
+        #send result back  
+        Write-Information -Tags "Audit" -MessageData $log
 }
